c977dee5aa18e360c1fed2c4a4940d6e207a2e0a
Filipe Alves <filipe.alvesrr@hotmail.com>
2023-02-16 11:23:10 -0300

    Adição dos models

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Category, Caso, Score, Donate
+# Models que aparecem na área administrativa
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Caso)
+class CasoAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Donate)
+class DonateAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Score)
+class ScoreAdmin(admin.ModelAdmin):
+    ...
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,48 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
+
+
+class Caso(models.Model):
+    title_of_case = models.CharField(max_length=100)
+    description = models.TextField()
+    value_total = models.FloatField()
+    value_received = models.FloatField()
+    date_expiration = models.DateField()
+    is_published = models.BooleanField(default=False)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+    usuario = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
+
+    def __str__(self):
+        return self.title_of_case
+
+
+class Donate(models.Model):
+    value_of_donate = models.FloatField()
+    date_of_donate = models.DateField()
+    caso = models.ForeignKey(
+        Caso, on_delete=models.SET_NULL, null=True
+    )
+    usuario = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
+
+
+class Score(models.Model):
+    value_of_score = models.IntegerField()
+    donate = models.ForeignKey(
+        Donate, on_delete=models.SET_NULL, null=True
+    )
+    usuario = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
