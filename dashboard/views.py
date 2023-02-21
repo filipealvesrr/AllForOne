ebaf9c3cabed2acdf8eab3a1e5a56c1b6f40b33e
Filipe Alves <filipe.alvesrr@hotmail.com>
2023-02-20 22:30:01 -0300

    Organização das pastas

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 
 
 def dashboard(request):
-    return render(request, 'dashboard/dashboard.html', context={
+    return render(request, 'dashboard/pages/dashboard.html', context={
         'is_dash': True,
     })
 
 
 def new_case(request):
-    return render(request, 'dashboard/new-case.html')
+    return render(request, 'dashboard/pages/new-case.html')
 
 
 def my_cases(request):
-    return render(request, 'dashboard/my-cases.html', context={
+    return render(request, 'dashboard/pages/my-cases.html', context={
         'is_dash': True,
     })
