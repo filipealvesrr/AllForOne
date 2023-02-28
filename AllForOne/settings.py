c53916e0fdb23d6abf1f6562f2de532c76aeb12f
Filipe Alves <filipe.alvesrr@hotmail.com>
2023-02-28 09:00:34 -0300

    ajustes settings

--- AllForOne/settings.py
+++ AllForOne/settings.py
@@ -81,13 +81,24 @@ WSGI_APPLICATION = 'AllForOne.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'BFu1USbqfak3TBrhsPkC',
+        'HOST': 'containers-us-west-32.railway.app',
+        'PORT': '7890',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
