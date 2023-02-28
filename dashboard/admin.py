03bc37d22dad989843c8eff1bc8a188cbd420ffa
Filipe Alves <filipe.alvesrr@hotmail.com>
2023-02-28 13:33:07 -0300

    Deve ser o último

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,29 +1,24 @@
 from django.contrib import admin
 from .models import Category, Caso, Score, Donate
-from django.views.decorators.csrf import csrf_exempt
 
 # Models que aparecem na área administrativa
 
 
-@csrf_exempt
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     ...
 
 
-@csrf_exempt
 @admin.register(Caso)
 class CasoAdmin(admin.ModelAdmin):
     ...
 
 
-@csrf_exempt
 @admin.register(Donate)
 class DonateAdmin(admin.ModelAdmin):
     ...
 
 
-@csrf_exempt
 @admin.register(Score)
 class ScoreAdmin(admin.ModelAdmin):
     ...
