bba638dff6dbcca92aa0adb9f37ce0446ee8d6a0
Filipe Alves <filipe.alvesrr@hotmail.com>
2023-02-28 10:26:44 -0300

    files statics again

--- AllForOne/settings.py
+++ AllForOne/settings.py
@@ -151,7 +151,7 @@ STATICFILES_DIRS = [
 ]
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
