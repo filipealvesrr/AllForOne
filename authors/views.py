c99cffeece2ea27c01b8199f9694d1cd78d4c995
Filipe Alves <filipe.alvesrr@hotmail.com>
2023-02-28 12:15:56 -0300

    Ajuda pai

--- authors/views.py
+++ authors/views.py
@@ -4,7 +4,7 @@ from .forms import RegisterForm, LoginForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
 
 def register_view(request):
@@ -38,7 +38,6 @@ def register_create(request):
     return redirect('authors:register')
 
 
-@csrf_exempt
 def login_view(request):
     form = LoginForm()
     return render(request, 'authors/pages/login.html', context={
@@ -46,7 +45,7 @@ def login_view(request):
     })
 
 
-@csrf_exempt
+@csrf_protect
 def login_create(request):
     if not request.POST:
         raise Http404()
